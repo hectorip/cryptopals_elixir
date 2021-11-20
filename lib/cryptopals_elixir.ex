0c63957a8c32a09bb31e4d2f84b2b1713640254b
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2021-11-19 23:39:55 -0600

    Guessing a Key

--- lib/cryptopals_elixir.ex
+++ lib/cryptopals_elixir.ex
@@ -36,4 +36,41 @@ defmodule CryptopalsElixir do
     <<bxor(a, b)>> <> _xor(rest1, rest2)
   end
   defp _xor(<<>>, <<>>), do: <<>>
+
+  def single_xor(<<a, rest1::binary()>>, <<b>>) do
+    <<bxor(a,b)>> <> single_xor(rest1, <<b>>)
+  end
+  def single_xor(<<>>, _), do: <<>>
+  @spec decrypt(binary, any) :: binary
+  def decrypt(input, key) do
+    bytes = Base.decode16!(input, case: :lower)
+    single_xor(bytes, key)
+  end
+  def test(input) do
+    0..127
+    |> Enum.map(fn k ->
+      kb = k |> Integer.to_string(16)
+      |> String.pad_leading(2, "0")
+      |> Base.decode16!()
+      result = decrypt(input, kb)
+      s = score(result)
+      ratio = s.letters / s.symbols
+      {k, result, ratio}
+    end)
+    |> Enum.sort(fn {_, _, a_ratio}, {_,_,b_ratio}->
+      a_ratio > b_ratio
+    end)
+    |> Enum.take(3)
+    |> IO.inspect
+  end
+
+  def score(input) do
+    input
+    |> String.downcase()
+    |> String.to_charlist()
+    |> Enum.reduce(%{symbols: 0, letters: 0}, fn c, acc ->
+      t = if c >= ?a and c <= ?z, do: :letters, else: :symbols
+      Map.update(acc, t, 0, fn x -> x + 1 end)
+    end)
+  end
 end
